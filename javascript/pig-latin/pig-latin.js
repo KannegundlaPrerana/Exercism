ce137478a6404874fad72cc786514ec30b5d1be9
KannegundlaPrerana <preranakannegundla4@gmail.com>
2020-08-05 22:46:41 +0530

    add another if-else and change code

--- javascript/pig-latin/pig-latin.js
+++ javascript/pig-latin/pig-latin.js
@@ -13,19 +13,22 @@ export class translator {
     if(vowels.indexOf(word[0]) != -1 || firstTwoCharacters == "xr" || firstTwoCharacters == "yt"){
       requiredIndex = 0;
     }
-    else if(word.indexOf("qu") != -1){
-      requiredIndex = word.indexOf("qu") + 2;
-    }
     else{
-      let index = 0 , length = word.length , yIndex = word.indexOf("y");
-      while(vowels.indexOf(word[index]) == -1 && index < length){
-        index ++;
+      if(word.indexOf("qu") != -1){
+        requiredIndex = word.indexOf("qu") + 2;
       }
-      if(index != 5){
-        requiredIndex = index;
-      }   
-      if((yIndex != -1) && (yIndex != 0) && (yIndex < requiredIndex)){
-        requiredIndex = yIndex;
+      else{
+        let index = 0 , length = word.length , yIndex = word.indexOf("y");
+        while(vowels.indexOf(word[index]) == -1 && index < length){
+          index ++;
+        }
+        if(index != length){
+          requiredIndex = index;
+        }  
+        if(((yIndex != -1) && (yIndex != 0)) && ((requiredIndex == undefined) || (yIndex < requiredIndex)))
+        {
+          requiredIndex = yIndex;
+        }
       }
     }
     return word.substring(requiredIndex,word.length) + word.substring(0,requiredIndex) + "ay";
